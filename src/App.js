9ab3d48d62988d93b6f66229943c62093630e147
yasmeenfoaud7-debug <yasmeenfoaud7@gmail.com>
2026-09-24 00:01:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function App() {
   const [customerName] = useState('ياسمين فؤاد');
 
 const categories = [
-  { id: 1, name: "لحوم", image: "images/borgr.jpg" }, // أو حسب مكانها
-  { id: 2, name: "مشويات", image: "images/meshwiyat.jpg" },
-  { id: 3, name: "بيتزا", image: "images/petza.jpg" },
-  { id: 4, name: "برجر", image: "images/burger-comb.jpg" },
-  { id: 5, name: "سوبر ماركت", image: "images/superma.jpg" },
-  { id: 6, name: "حلويات", image: "images/halwayat.jpg" },
-  { id: 7, name: "أكل بيتي", image: "images/homemad.jpg" },
-  { id: 8, name: "شاورما", image: "images/shwrmajpg.jpg" },
-  { id: 9, name: "معجنات", image: "images/stuffed-veggies.jpg" }, // أو الصورة المناسبة للممعجنات
-  { id: 10, name: "صيدلية", image: "images/pharmacy.jpg" },
-  { id: 11, name: "فواكه", image: "images/fruit.jpg" },
-  { id: 12, name: "خضروات", image: "images/grocery-bag.jpg" }
+  { id: 1, name: "لحوم", image: "/images/borgr.jpg" },
+  { id: 2, name: "مشويات", image: "/images/meshwiyat.jpg" },
+  { id: 3, name: "بيتزا", image: "/images/petza.jpg" },
+  { id: 4, name: "برجر", image: "/images/burger-comb.jpg" },
+  { id: 5, name: "سوبر ماركت", image: "/images/superma.jpg" },
+  { id: 6, name: "حلويات", image: "/images/halwayat.jpg" },
+  { id: 7, name: "أكل بيتي", image: "/images/homemad.jpg" },
+  { id: 8, name: "شاورما", image: "/images/shwrmajpg.jpg" },
+  { id: 9, name: "معجنات", image: "/images/stuffed-veggies.jpg" },
+  { id: 10, name: "صيدلية", image: "/images/pharmacy.jpg" },
+  { id: 11, name: "فواكه", image: "/images/fruit.jpg" },
+  { id: 12, name: "خضروات", image: "/images/grocery-bag.jpg" }
 ];
   
 
